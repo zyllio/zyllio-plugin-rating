0c8db6b6f04a8fdc45a674f4cb568c43bbc618a6
Zyllio <96659995+zyllio@users.noreply.github.com>
2022-01-06 16:40:22 +0100

    -

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# zyllio-plugin-rating
-A rating component based on Zyllio SDK
+# Zyllio Plugin Meme Generator
+
+This Zyllio plugin implements a simple Component Rating
+
+The property `value` gives the number of stars
+
+# Demonstration
+
+<img src="./snapshots/demo.gif">
+
+# Development environment
+
+Node.js 14+ is required
+
+# Build
+
+As this project is based on pure Javascript, it does not need compilation process
+
+Run this command to get experiment using Zyllio SDK 
+
+```shell
+npm install
+```
+
+# Install
+
+Only rating.js file is to be installed within Zyllio Studio under Designer/Plugins panel
+
+# Fork this plugin
+
+You are welcome to clone and fork this repository, looking forward your creativity 😀
+
+# Support
+
+Zyllio team is available for any support, feature requests and questions
+
+- email: contact@zyllio.com
+- twitter: [@zyllio](https://twitter.com/zyllio) 
--- src/rating.js
+++ src/rating.js
@@ -33,7 +33,7 @@ class RatingComponent extends HTMLElement {
 
   init() {
 
-    const propertyValue = zySdk.services.factory.getPropertyValue(this, 'value')
+    const propertyValue = zySdk.services.component.getPropertyValue(this, 'value')
 
     zySdk.services.dictionary.onChange(propertyValue, () => {
       this.refresh()
@@ -54,7 +54,7 @@ class RatingComponent extends HTMLElement {
 
       const element = this.shadowRoot.querySelector('div')
   
-      const propertyValue = zySdk.services.factory.getPropertyValue(this, 'value')
+      const propertyValue = zySdk.services.component.getPropertyValue(this, 'value')
 
       const value = zySdk.services.dictionary.getValue(propertyValue)
     
@@ -63,7 +63,7 @@ class RatingComponent extends HTMLElement {
   }
 }
 
-const IconData = `
+const Icon = `
   <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24" fill="#cccccc">    
     <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
   </svg>
@@ -71,7 +71,7 @@ const IconData = `
 
 const RatingMetadata = {
   id: 'custom-rating',
-  icon: IconData,
+  icon: Icon,
   label: 'Rating',
   category: 'Basics',
   subCategory: 'Plugins',
