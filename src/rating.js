0afd3f7cdf4412904f58fe4d47b4627a95987d66
Zyllio <96659995+zyllio@users.noreply.github.com>
2022-08-24 23:08:10 +0200

    -

--- src/rating.js
+++ src/rating.js
@@ -1,8 +1,10 @@
 /// <reference types="@zyllio/zy-sdk" />
 
-console.log('Plugin Rating started')
+(function () {
 
-const HtmlContent = `
+  console.log('Plugin Rating started')
+
+  const HtmlContent = `
 
   <style>
     :host {
@@ -18,78 +20,80 @@ const HtmlContent = `
   <div></div>                       
 `;
 
-class RatingComponent extends HTMLElement {
+  class RatingComponent extends HTMLElement {
 
-  constructor() {
-    super()
+    constructor() {
+      super()
 
-    this.shadow = this.attachShadow({ mode: 'open' });
-  }
+      this.shadow = this.attachShadow({ mode: 'open' });
+    }
 
-  connectedCallback() {
+    connectedCallback() {
 
-    this.shadow.innerHTML = HtmlContent;
+      this.shadow.innerHTML = HtmlContent;
 
-    setTimeout( () => { this.init() })
+      setTimeout(() => { this.init() })
 
-    this.refresh()
-  }
-
-  init() {
-
-    const propertyValue = zySdk.services.component.getPropertyValue(this, 'value')
-
-    zySdk.services.dictionary.onChange(propertyValue, () => {
       this.refresh()
-    })
-  }
+    }
 
-  static get observedAttributes() {
-    return ['data-value'];
-  }
+    init() {
 
-  attributeChangedCallback() {
-    this.refresh()
-  }
-
-  refresh() {
-
-    setTimeout( () => { 
-
-      const element = this.shadowRoot.querySelector('div')
-  
       const propertyValue = zySdk.services.component.getPropertyValue(this, 'value')
 
-      const value = zySdk.services.dictionary.getValue(propertyValue)
-    
-      element.innerText = '⭐'.repeat(parseInt(value))
-    })
-  }
-}
+      zySdk.services.dictionary.onChange(propertyValue, () => {
+        this.refresh()
+      })
+    }
 
-const Icon = `
+    static get observedAttributes() {
+      return ['data-value'];
+    }
+
+    attributeChangedCallback() {
+      this.refresh()
+    }
+
+    refresh() {
+
+      setTimeout(() => {
+
+        const element = this.shadowRoot.querySelector('div')
+
+        const propertyValue = zySdk.services.component.getPropertyValue(this, 'value')
+
+        const value = zySdk.services.dictionary.getValue(propertyValue)
+
+        element.innerText = '⭐'.repeat(parseInt(value))
+      })
+    }
+  }
+
+  const Icon = `
   <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24" fill="#cccccc">    
     <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
   </svg>
 `;
 
-const RatingMetadata = {
-  id: 'custom-rating',
-  icon: Icon,
-  label: 'Rating',
-  category: 'Plugins',
-  subCategory: 'Ratings',
-  hidden: false,
-  keepRatio: false,
-  properties: [{
-    id: 'value',
-    name: 'Value',
-    type: 'number',
-    tootip: 'The rating between 1 and 5',
-    default: '3',
-    main: true
-  }],
-  styles: []
-}
+  const RatingMetadata = {
+    id: 'custom-rating',
+    icon: Icon,
+    label: 'Rating',
+    category: 'Plugins',
+    subCategory: 'Ratings',
+    hidden: false,
+    keepRatio: false,
+    properties: [{
+      id: 'value',
+      name: 'Value',
+      type: 'number',
+      tootip: 'The rating between 1 and 5',
+      default: '3',
+      main: true
+    }],
+    styles: []
+  }
 
-zySdk.services.registry.registerComponent(RatingMetadata, RatingComponent)
+  zySdk.services.registry.registerComponent(RatingMetadata, RatingComponent)
+
+})();
